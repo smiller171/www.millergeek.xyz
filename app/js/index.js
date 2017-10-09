21f6862388cc39f7414b1dadccaf09af2cb15043
Scott Miller <scott@millergeek.xyz>
2017-10-09 19:43:44 -0400

    lint

--- app/js/index.js
+++ app/js/index.js
@@ -3,55 +3,56 @@ $(function () {
   $("[data-toggle=\"tooltip\"]").tooltip();
 });
 
-$("#contactForm").submit(function(e) {
+$("#contactForm").submit(function (e) {
   e.preventDefault();
-  $.post( "https://api.millergeek.xyz/sendmail", {
+  $.post("https://api.millergeek.xyz/sendmail", {
     "bodyData": $("#contactBody").val(),
     "subjectData": $("#contactSubject").val(),
     "replyToAddresses": [$("#contactEmail").val()]
-  })
-  .done(function() {
+  }).done(function () {
     console.log("sent successfully");
     $("#emailSuccess").addClass("show");
-    setTimeout( function(){ $("#emailSuccess").removeClass("show"); }, 2000 );
-  })
-  .fail(function(err) {
+    setTimeout(function () {
+      $("#emailSuccess").removeClass("show");
+    }, 2000);
+  }).fail(function (err) {
     console.error("Failed: ", err);
     $("#emailFail").addClass("show");
-    setTimeout( function(){ $("#emailFail").removeClass("show"); }, 2000 );
+    setTimeout(function () {
+      $("#emailFail").removeClass("show");
+    }, 2000);
   });
 });
 
 // Ripple-effect animation
-$(function($) {
-  $(".ripple-effect").click(function(e){
+$(function ($) {
+  $(".ripple-effect").click(function (e) {
     var rippler = $(this);
 
-        // create .ink element if it doesn't exist
-    if(rippler.find(".ink").length == 0) {
+    // create .ink element if it doesn't exist
+    if (rippler.find(".ink").length == 0) {
       rippler.append("<span class='ink'></span>");
     }
 
     var ink = rippler.find(".ink");
 
-        // prevent quick double clicks
+    // prevent quick double clicks
     ink.removeClass("animate");
 
-        // set .ink diametr
-    if(!ink.height() && !ink.width())
-        {
+    // set .ink diametr
+    if (!ink.height() && !ink.width()) {
       var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
       ink.css({height: d, width: d});
     }
 
-        // get click coordinates
-    var x = e.pageX - rippler.offset().left - ink.width()/2;
-    var y = e.pageY - rippler.offset().top - ink.height()/2;
+    // get click coordinates
+    var x = e.pageX - rippler.offset().left - ink.width() / 2;
+    var y = e.pageY - rippler.offset().top - ink.height() / 2;
 
-        // set .ink position and add class .animate
+    // set .ink position and add class .animate
     ink.css({
-      top: y+"px",
-      left:x+"px"
+      top: y + "px",
+      left: x + "px"
     }).addClass("animate");
   });
 });
