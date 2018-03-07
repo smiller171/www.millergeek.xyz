79d28590ac859f5d222ffc5839be54f3194c46a9
Scott Miller <scott@millergeek.xyz>
2018-03-07 17:57:47 -0500

    styling about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,6 +1,7 @@
 <template>
-  <main>
-    <about-summary/>
+  <main class="main">
+    <about-summary class="summary"/>
+    <div class="cloud"/>
   </main>
 </template>
 
@@ -13,3 +14,26 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 1rem;
+}
+.summary {
+  max-width: 390px;
+  flex-basis: 300px;
+  flex-shrink: 3;
+  flex-grow: 3;
+  padding-right: 2rem;
+}
+.cloud {
+  max-width: 530px;
+  flex-basis: 500px;
+  flex-grow: 5;
+  flex-shrink: 5;
+}
+</style>
+
