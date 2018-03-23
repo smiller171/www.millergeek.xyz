683533a40aef4c05eabbe7bb785dec7836493c9a
Scott Miller <scott@millergeek.xyz>
2018-03-23 16:55:21 -0400

    fix theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Personal Website' },
-      { theme_color: '#691b99' }
+      { name: 'theme-color', content: '#691b99'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
