2206bc12f4fc2d4425703c30239db6c9fe1ee699
Scott Miller <scott@millergeek.xyz>
2018-03-28 15:31:56 -0400

    fix netlify form

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -6,20 +6,24 @@
           name="contact"
           method="POST"
           netlify
+          @submit.prevent="handleSubmit"
         >
           <h2>Contact Me</h2>
           <v-text-field
             id="contact-email"
+            v-model="form.email"
             name="email"
             label="Email Address"
           />
           <v-text-field
             id="contact-subject"
+            v-model="form.subject"
             name="subject"
             label="Subject"
           />
           <v-text-field
             id="contact-comments"
+            v-model="form.comments"
             name="comments"
             label="Comments"
             multi-line
@@ -36,6 +40,39 @@
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        subject: '',
+        comments: ''
+      }
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&');
+    },
+    handleSubmit() {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({ 'form-name': 'contact', ...this.form }),
+      })
+        .then(() => alert('Success!'))
+        .catch(error => alert(error));
+    }
+  }
+};
+</script>
+
 
 <style lang="scss" scoped>
 .contactContainer {
