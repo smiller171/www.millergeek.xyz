56b656c20bfe94911cb76081195e79285ee0cdfd
Scott Miller <scott@millergeek.xyz>
2018-04-12 11:38:17 -0400

    make required fields work

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -3,6 +3,7 @@
     <div class="contactContainer">
       <v-card class="contactCard">
         <v-form
+          v-model="valid"
           name="contact"
           method="POST"
           @submit.prevent="handleSubmit"
@@ -11,6 +12,7 @@
           <v-text-field
             id="contact-email"
             v-model="form.email"
+            :rules="emailRules"
             name="email"
             label="Email Address"
             required
@@ -24,6 +26,7 @@
           <v-text-field
             id="contact-message"
             v-model="form.message"
+            :rules="messageRules"
             name="message"
             label="Message"
             multi-line
@@ -31,6 +34,7 @@
             required
           />
           <v-btn
+            :disabled="!valid"
             color="success"
             type="submit"
           >
@@ -50,7 +54,15 @@ export default {
         email: '',
         subject: '',
         message: ''
-      }
+      },
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(v) || 'E-mail must be valid'
+      ],
+      messageRules: [
+        v => !!v || 'Message is required'
+      ]
     };
   },
   methods: {
